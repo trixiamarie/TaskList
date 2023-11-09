654e5b4f5716dd3b2ed85be01333fa1665b07396
trixiamarie <trixialorenzana@gmail.com>
2023-11-09 23:40:33 +0100

    Esercizio Giorno 13-S3

--- Esercizio Giorno 13-S3/js/ds.js
+++ Esercizio Giorno 13-S3/js/ds.js
@@ -1,66 +1,70 @@
-
 let name1 = document.querySelector('#nameForm input[name="nome"]');
 let saluto = document.querySelector("#salutoNome");
-let salutobtn = document.querySelector("#salutobtn"); 
-
+let salutobtn = document.querySelector("#salutobtn");
 
 salutobtn.addEventListener("click", addName);
 
 function addName(event) {
-    event.preventDefault();
-    let existingP = saluto.querySelector("p");
-    if(name1.value.trim() !== ''){
-    if (existingP){
-        existingP.remove();
-        let newP = document.createElement("p");
-        newP.innerText = 'Ciao, ' + name1.value ;  
-        saluto.appendChild(newP);
-        name1.value = '';
-    } else{ 
-    let newP = document.createElement("p");
-    newP.innerText = "Ciao, " + name1.value;  
-    saluto.appendChild(newP);
-    name1.value = '';
-    }
-    }else{
-        alert("Inserisci il tuo nome! Non sei colui-che-non-può-essere-nominato!");
-    }
+  event.preventDefault();
+  let existingP = saluto.querySelector("p");
+  if (name1.value.trim() !== "") {
+    /*if (existingP) {*/
+      
+      let newP = document.createElement("p");
+      newP.innerText = "Ciao, " + name1.value;
+      name1.style.color= `#963f5f`;
+      saluto.appendChild(newP);
+      name1.value = "";
+      existingP.remove();
+    /*} else {
+      let newP = document.createElement("p");
+      newP.innerText = "Ciao, " + name1.value;
+      saluto.appendChild(newP);
+      name1.value = "";
+      name1.style.color= `#963f5f`;
+      console.log("pippo e pluto");
+    }*/
+  } else {
+    alert("Inserisci il tuo nome! Non sei colui-che-non-può-essere-nominato!");
+  }
 }
 
-
 let todoForm = document.querySelector('#todoForm input[name="task"]');
-let formBtn = document.querySelector("#formBtn"); 
+let formBtn = document.querySelector("#formBtn");
 let tasklist = document.querySelector("#tasklist");
 
 formBtn.addEventListener("click", addTask);
 
+function addTask(event) {
+  event.preventDefault();
+  if (todoForm.value.trim() !== "") {
+    let newP = document.createElement("p");
+    newP.innerText = todoForm.value;
+    tasklist.appendChild(newP);
 
+    let existingP = tasklist.querySelectorAll("p");
 
-function addTask(event){
-    event.preventDefault();
-    let existingP = tasklist.querySelector("p");
-    if(todoForm.value.trim() !== ''){
-        if (!existingP){
-            let newP = document.createElement("p");
-            newP.innerText = todoForm.value+ " ";  
-            tasklist.appendChild(newP);
-            todoForm.value = '';
-            newP.addEventListener("click",taskDone);
-            function taskDone(){
-                newP.style.textDecorationLine= "line-through";
-            }
-            let removeBtn = document.createElement("button");
-            removeBtn.innerText = "remove";
-            newP.appendChild(removeBtn);
-            removeBtn.addEventListener("click", cancellaTask);
-            function cancellaTask(event){
-                event.preventDefault();
-                newP.remove();
-            }
-        } else {
-            alert("Hai già inserito questa task!");
-        }
-    }else {
-        alert("Inserisci una task prima di aggiungere!");
+    for (let i = 0; i < existingP.length - 1; i++) {
+      if (todoForm.value + "remove" === existingP[i].innerText) {
+        alert("Hai già inserito questa task!");
+        newP.remove();
+      }
     }
+    todoForm.value = "";
+    newP.addEventListener("click", taskDone);
+    function taskDone() {
+      newP.style.textDecorationLine = "line-through";
+      newP.style.color = `#963f5f`;
+    }
+    let removeBtn = document.createElement("button");
+    removeBtn.innerText = "remove";
+    newP.appendChild(removeBtn);
+    removeBtn.addEventListener("click", cancellaTask);
+    function cancellaTask(event) {
+      event.preventDefault();
+      newP.remove();
+    }
+  } else {
+    alert("Inserisci una task prima di aggiungere!");
+  }
 }
