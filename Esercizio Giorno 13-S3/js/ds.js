6ea936388798027f344e8e9cf80133ce981ab9cf
trixiamarie <trixialorenzana@gmail.com>
2023-11-10 10:08:24 +0100

    Esercizio Giorno 13-S3

--- Esercizio Giorno 13-S3/js/ds.js
+++ Esercizio Giorno 13-S3/js/ds.js
@@ -6,28 +6,27 @@ salutobtn.addEventListener("click", addName);
 
 function addName(event) {
   event.preventDefault();
-  let existingP = saluto.querySelector("p");
+
+  let existingP = saluto.querySelectorAll("p");
+
+  if (existingP.length > 0) {
+    existingP.forEach(p => p.remove());
+  }
+
   if (name1.value.trim() !== "") {
-    /*if (existingP) {*/
-      
-      let newP = document.createElement("p");
-      newP.innerText = "Ciao, " + name1.value;
-      name1.style.color= `#963f5f`;
-      saluto.appendChild(newP);
-      name1.value = "";
-      existingP.remove();
-    /*} else {
-      let newP = document.createElement("p");
-      newP.innerText = "Ciao, " + name1.value;
-      saluto.appendChild(newP);
-      name1.value = "";
-      name1.style.color= `#963f5f`;
-      console.log("pippo e pluto");
-    }*/
+    let newP = document.createElement("p");
+    let newP1 = document.createElement("p");
+    newP.innerText = "Ciao, ";
+    newP1.innerText = name1.value;
+    newP1.style.color = `#8f458a`;
+    saluto.appendChild(newP);
+    saluto.appendChild(newP1);
+    name1.value = "";
   } else {
     alert("Inserisci il tuo nome! Non sei colui-che-non-può-essere-nominato!");
   }
 }
+
 
 let todoForm = document.querySelector('#todoForm input[name="task"]');
 let formBtn = document.querySelector("#formBtn");
@@ -51,19 +50,34 @@ function addTask(event) {
       }
     }
     todoForm.value = "";
-    newP.addEventListener("click", taskDone);
-    function taskDone() {
-      newP.style.textDecorationLine = "line-through";
-      newP.style.color = `#963f5f`;
+    newP.addEventListener("click", taskToggle);
+
+    function taskToggle() {
+      if (newP.style.textDecorationLine === "line-through") {
+        newP.style.textDecorationLine = "none";
+        newP.style.color = "black"; 
+        } else {
+        newP.style.textDecorationLine = "line-through";
+        newP.style.color = `#963f5f`;
+      }
     }
+
     let removeBtn = document.createElement("button");
     removeBtn.innerText = "remove";
     newP.appendChild(removeBtn);
     removeBtn.addEventListener("click", cancellaTask);
     function cancellaTask(event) {
       event.preventDefault();
-      newP.remove();
+      let cancella = confirm("Sei sicuro di voler cancellare questa task?");
+      if (cancella === true){
+        newP.remove();
+      }
+      else{
+        newP.style.textDecorationLine = "none";
+        newP.style.color = "black"; 
+      }
     }
+
   } else {
     alert("Inserisci una task prima di aggiungere!");
   }
